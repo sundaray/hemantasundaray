b30023cc64905d8526d21cb99795142c52cf0400
sundaray <pgcim14.hemant@spjimr.org>
2024-07-11 08:59:18 +0530

    installed react components

--- fonts.ts
+++ fonts.ts
@@ -1,13 +1,6 @@
-import { Playfair_Display } from "next/font/google";
-import { Protest_Guerrilla } from "next/font/google";
+import { Playfair_Display } from "next/font/google"
 
 export const playfair_display = Playfair_Display({
   subsets: ["latin"],
   display: "swap",
-});
-
-export const protest_guerrilla = Protest_Guerrilla({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-});
+})
