a62404610395e319badfd7d7948370e89333b007
sundaray <pgcim14.hemant@spjimr.org>
2024-09-15 19:45:05 +0200

    dot removed

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ export default function HomePage() {
     <div className="container max-w-3xl">
       <h1 className="mb-4 text-4xl font-bold">About</h1>
       <p className="text-lg">
-        I'm Hemanta Sundaray, a full-stack developer and technical writer.
+        Coming soon.
       </p>
     </div>
   )
