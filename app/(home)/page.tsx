849ca7054fa8cc22a91be17d0039d78594841207
sundaray <pgcim14.hemant@spjimr.org>
2024-10-05 09:27:48 +0200

    added project

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,8 @@
 export default function HomePage() {
   return (
     <div className="container max-w-3xl">
-      <h1 className="mb-4 text-3xl lg:text-4xl font-semibold">Hello World!</h1>
-      <p className="lg:text-lg text-muted-foreground">
+      <h1 className="mb-4 text-2xl font-normal">Hello world!</h1>
+      <p className="text-base text-slate-600">
         I'm Hemanta Sundaray, a full-stack developer and technical writer.
       </p>
     </div>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,26 @@
+import { Icons } from "@/components/icons"
+
 export default function ProjectsPage() {
   return (
-    <div className="container max-w-3xl">
+    <div className="container max-w-xl">
       <h1 className="mb-4 text-4xl font-bold">Projects</h1>
-      <p className="text-lg">Coming soon.</p>
+      <div className="rounded-md border bg-slate-50 p-4 md:p-6">
+          <a 
+            href="https://www.makeaudio.app" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            className="group flex items-center gap-2 hover:underline transition"
+          >
+            <h2 className="text-xl font-semibold text-slate-900">
+              www.makeaudio.app
+            </h2>
+            <Icons.externalLink className="inline-block size-4 text-slate-500 transition-transform group-hover:translate-x-0.5 group-hover:-translate-y-0.5" aria-hidden="true" />
+            <span className="sr-only">(opens in a new tab)</span>
+          </a>
+        <p className="text-slate-600">Convert text to audio</p>
+        <p className="mt-4 text-sm text-slate-500">Launched in March, 2024</p>
+        <p className="text-sm text-slate-500">Total revenue till date: $105</p>
+      </div>
     </div>
   )
 }
